788135ddbf06ae4caf9c222060610e1959f6c753
Hemanth Varma <mokahemanthkumar999@gmail.com>
2022-12-09 21:40:54 +0530

    some changes made in main file

--- main.tf
+++ main.tf
@@ -1,117 +1,61 @@
 provider "aws" {
-    access_key = "${var.aws_access_key}"
-    secret_key = "${var.aws_secret_key}"
-    region = "${var.aws_region}"
+    access_key = "AKIA2KQBDCDGJ47EVDOK"
+    secret_key = "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"
+    region = "us-east-1"
 }
 
-resource "aws_vpc" "Block-1" {
-    cidr_block = "${var.vpc_cidr}"
+resource "aws_vpc" "vpc-block" {
+    cidr_block = "192.168.0.0/16"
     enable_dns_hostnames = true
     tags = {
-        Name = "${var.vpc_name}"
+        Name = "myvpc"
 	Owner = "Hemanth Varma"
-	environment = "${var.environment}"
+	environment = "dev"
     }
 }
 
-resource "aws_internet_gateway" "Block-2" {
-    vpc_id = "${aws_vpc.Block-1.id}"
-	tags = {
-        Name = "${var.IGW_name}"
+resource "aws_internet_gateway" "IGW-block" {
+    vpc_id = "${aws_vpc.vpc-block.id}"
+    tags = {
+        Name = "myigw"
     }
+  
 }
 
-resource "aws_subnet" "subnet1-public" {
-    vpc_id = "${aws_vpc.Block-1.id}"
-    cidr_block = "${var.public_subnet1_cidr}"
+resource "aws_subnet" "public-subnet1" {
+    vpc_id = "${aws_vpc.vpc-block.id}"
+    cidr_block = "192.168.0.0/24"
     availability_zone = "us-east-1a"
-
     tags = {
-        Name = "${var.public_subnet1_name}"
+        Name = "mypubsub1"
     }
+
 }
 
-resource "aws_subnet" "subnet2-public" {
-    vpc_id = "${aws_vpc.Block-1.id}"
-    cidr_block = "${var.public_subnet2_cidr}"
-    availability_zone = "us-east-1b"
-
+resource "aws_subnet" "public-subnet2" {
+    vpc_id = "${aws_vpc.vpc-block.id}"
+    cidr_block = "192.168.1.0/24"
+    availability_zone = "us-east-1a"
     tags = {
-        Name = "${var.public_subnet2_name}"
-    }
-}
-
-resource "aws_subnet" "subnet3-public" {
-    vpc_id = "${aws_vpc.Block-1.id}"
-    cidr_block = "${var.public_subnet3_cidr}"
-    availability_zone = "us-east-1c"
-
-    tags = {
-        Name = "${var.public_subnet3_name}"
-    }
-	
-}
-
-
-resource "aws_route_table" "terraform-public" {
-    vpc_id = "${aws_vpc.Block-1.id}"
-
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = "${aws_internet_gateway.Block-2.id}"
+        Name = "mypubsub2"
     }
 
-    tags = {
-        Name = "${var.Main_Routing_Table}"
-    }
 }
 
-resource "aws_route_table_association" "terraform-public" {
-    subnet_id = "${aws_subnet.subnet1-public.id}"
-    route_table_id = "${aws_route_table.terraform-public.id}"
-}
+resource "aws_route_table" "RT-Block" {
+  vpc_id = aws_vpc.vpc-block.id
 
-resource "aws_security_group" "allow_all" {
-  name        = "allow_all"
-  description = "Allow all inbound traffic"
-  vpc_id      = "${aws_vpc.Block-1.id}"
-
-  ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.IGW-block.id
   }
 
-  egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
-    }
+  tags = {
+    Name = "myrt"
+  }
 }
 
-# data "aws_ami" "my_ami" {
-#      most_recent      = true
-#      #name_regex       = "^mavrick"
-#      owners           = ["721834156908"]
-# }
-
-
-# resource "aws_instance" "web-1" {
-#     ami = var.imagename
-#     #ami = "ami-0d857ff0f5fc4e03b"
-#     #ami = "${data.aws_ami.my_ami.id}"
-#     availability_zone = "us-east-1a"
-#     instance_type = "t2.micro"
-#     key_name = "LaptopKey"
-#     subnet_id = "${aws_subnet.subnet1-public.id}"
-#     vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
-#     associate_public_ip_address = true	
-#     tags = {
-#         Name = "Server-1"
-#         Env = "Prod"
-#         Owner = "Sree"
-# 	CostCenter = "ABCD"
-#     }
-#   }
+resource "aws_route_table_association" "RT-Block" {
+    subnet_id = "${aws_subnet.public-subnet1.id}"
+    route_table_id = "${aws_route_table.RT-Block.id}"
+}
