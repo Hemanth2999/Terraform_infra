e8cd204d91eee9258eee21713833cce247643c6a
Hemanth2999 <116809551+Hemanth2999@users.noreply.github.com>
2022-12-16 14:38:16 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,12 +3,10 @@ variable "aws_secrete_key" {}
 variable "aws_region" {}
 variable "amis" {
     description = "AMIs by region"
-    default = {
-        us-east-1a = "ami-0b0dcb5067f052a63" # ubuntu 14.04 LTS
-    }
+    default = "ami-0b0dcb5067f052a63" # ubuntu 14.04 LTS
 }
 variable "vpc_name" {}
-variable "aws_cider_block" {}
+variable "aws_cidr_block" {}
 variable "aws_igw_name" {}
 variable "aws_public_subnet1_name" {}
 variable "aws_public_subnet1_cidr" {}
@@ -19,11 +17,9 @@ variable "aws_security_group" {}
 variable "aws_key_name" {}
 variable "azs" {
   description = "Run the EC2 Instances in these Availability Zones"
-  default = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  default = "us-east-1a"
 }
 variable "environment" { default = "dev" }
 variable "instance_type" {
-  default = {
-    dev = "t2.micro"
+  default = "t2.micro"
      }
-}
