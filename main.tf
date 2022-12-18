5163cd427f00c1ea5665006be4ac53a3c8cc30b4
Hemanth Varma <mokahemanthkumar999@gmail.com>
2022-12-18 09:10:11 +0530

    my new infra

--- main.tf
+++ main.tf
@@ -85,7 +85,7 @@ resource "aws_instance" "My-web" {
   ami = "${var.amis}"
   availability_zone = "${var.azs}"
   instance_type = "t2.micro"
-  key_name = "${var.aws_key_name}"
+  key_name = "${var.key_name}"
   subnet_id = "${aws_subnet.public-subnet1.id}"
   vpc_security_group_ids = [ "${aws_security_group.SG-Block.id}" ]
   associate_public_ip_address = "true"
